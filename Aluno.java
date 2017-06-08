b791e87fa82062c48c4f15c26af99da1a6f36725
Marco Aurelio <profmarcosesteves@gmail.com>
2017-06-08 18:18:21 -0300

    curso de git

---
+++ Aluno.java
@@ -0,0 +1,47 @@
+package br.com.uva.academico;
+
+import br.com.uva.gerencial.Curso;
+
+public class Aluno extends Curso{
+	private static int mat_aux = 0;
+	private int mat = 0;
+	private String nome;
+	private Curso curso;
+	
+	
+	
+	public int getMat() {
+		return mat;
+	}
+
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public Aluno(){
+		mat = mat_aux++;
+	}
+	try{
+		public void inscreverAluno(Curso curso){
+			
+				if(this.curso == null){
+					
+				}
+			}catch{
+				System.out.println(this.nome + "já está cursando " + this.curso);
+			}
+			
+
+	public void listarCurso(){
+		this.toString();
+	}
+}
